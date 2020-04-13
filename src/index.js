7e86bf0a343f7bf37f256661d2bfd2e1bc40b103
ivan hoinych <hoinych@gmail.com>
2020-04-13 20:45:18 +0300

    few steps further

--- src/index.js
+++ src/index.js
@@ -1,42 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-// import App from './App';
-import * as serviceWorker from './serviceWorker';
+import App from './components/app/';
 
 
-const Header = () => {
-	return <h2>Hello World!</h2>
-}
-
-const Field = () => {
-	return <input type='text' placeholder='type here' />
-}
-
-const Btn = () => {
-	const text = 'Log in';
-	const res = () => {
-		return 'Log in, please'
-	}
-	return <button>{res()}</button>
-}
-
-const App = () => {
-	return (
-		<div>
-			<Header />
-			<Field />
-			<Btn />
-		</div>
-	)
-}
-
-ReactDOM.render(
-	<App />,
-	document.getElementById('root')
-);
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+ReactDOM.render(<App />, document.getElementById('root'));
